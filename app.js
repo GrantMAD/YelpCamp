12370f6d57430d993e4cf99032b2f97a283fb352
Grant Davidson <grant_dude1@hotmail.com>
2018-09-19 13:53:47 +0000

    added mLab

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
-mongoose.connect("mongodb://localhost/yelp_camp_v13", { useNewUrlParser: true });
+//mongoose.connect("mongodb://localhost/yelp_camp_v13", { useNewUrlParser: true });
+mongoose.connect("mongodb://Grant:simba6908@ds163162.mlab.com:63162/myyelpcamp", { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
